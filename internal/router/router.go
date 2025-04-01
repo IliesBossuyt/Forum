0781b662703e0c7a08de330ebd90000d11dc4278
IliesBossuyt <ilies.bossuyt@ynov.com>
2025-04-01 09:29:10 +0200

    Branch

--- internal/router/router.go
+++ internal/router/router.go
@@ -21,23 +21,24 @@ func Router() {
 	requireRole := security.RequireRole
 
 	// === Fichiers statiques ===
-	mainRouter.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public/static"))))
+	mainRouter.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../public/static"))))
 
 	// === Guest routes ===
 	guestRouter := http.NewServeMux()
 	guestRouter.HandleFunc("/", handlers.Accueil)
 	guestRouter.HandleFunc("/home", handlers.Home)
-	mainRouter.Handle("/", http.StripPrefix("/", guestRouter))
+	guestRouter.HandleFunc("/image/", handlers.GetImage)
+	mainRouter.Handle("/entry/", requireRole("guest", "user", "admin", "moderator")( http.StripPrefix("/entry", guestRouter)))
 
 	// === Auth routes ===
 	authRouter := http.NewServeMux()
 	authRouter.HandleFunc("/register", handlers.Register)
 	authRouter.HandleFunc("/login", handlers.Login)
 	authRouter.HandleFunc("/logout", handlers.Logout)
-	authRouter.HandleFunc("/auth/google/login", security.GoogleLogin)
-	authRouter.HandleFunc("/auth/google/callback", security.GoogleCallback)
-	authRouter.HandleFunc("/auth/github/login", security.GitHubLogin)
-	authRouter.HandleFunc("/auth/github/callback", security.GitHubCallback)
+	authRouter.HandleFunc("/google/login", security.GoogleLogin)
+	authRouter.HandleFunc("/google/callback", security.GoogleCallback)
+	authRouter.HandleFunc("/github/login", security.GitHubLogin)
+	authRouter.HandleFunc("/github/callback", security.GitHubCallback)
 	mainRouter.Handle("/auth/", http.StripPrefix("/auth", authRouter))
 
 	// === User routes ===
@@ -47,8 +48,7 @@ func Router() {
 	userRouter.HandleFunc("/like", handlers.LikePost)
 	userRouter.HandleFunc("/edit-post", handlers.EditPost)
 	userRouter.HandleFunc("/delete-post", handlers.DeletePost)
-	userRouter.HandleFunc("/image/", handlers.GetImage)
-	mainRouter.Handle("/user/", requireRole("user")(http.StripPrefix("/user", userRouter)))
+	mainRouter.Handle("/user/", requireRole("user", "admin", "moderator")(http.StripPrefix("/user", userRouter)))
 
 	// === Admin routes (Role: Admin) ===
 	adminRouter := http.NewServeMux()
